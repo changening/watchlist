e5ed52082c121b05492bd1952bd1d575e3e37763
changening <1044220154@qq.com>
2022-08-17 19:38:39 +0800

    create a database

--- app.py
+++ app.py
@@ -1,30 +1,83 @@
+import os
+import sys
+import click
+
 from flask import Flask,render_template
 from markupsafe import escape
 from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
 
-name = '小泽'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+# SQLite URI 兼容配置
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'   # 如果是 Windows 系统，使用三个斜线
+else:
+    prefix = 'sqlite:////'  # 否则使用四个斜线
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+
+# 表名将会是 user（自动生成，小写处理）
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+
+# 表名将会是 movie
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+# 自定义命令来自动执行创建数据库表操作
+@app.cli.command()    # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    if drop:          # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.') # 输出提示信息
+
+
+# 生成虚拟数据到数据库
+@app.cli.command()
+def forge():
+    db.create_all()
+    name = 'changing'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    # 定义user的名字后提交到数据库
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('data were created')
+
 
 @app.route('/')
-# @app.route('/home')
-# @app.route('/index')
-# def hello():
-#     return '<h1>你好小屁孩</h1><img src="https://up.54fcnr.com/pic_source/72/c8/f0/72c8f01c9b417bf07708e0373c645a12.gif">'
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
+
 
 @app.route('/user/<name>')
 def user_page(name):
