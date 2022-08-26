d376cd9439de0068cc07d69ceda4977d25498a8c
changening <1044220154@qq.com>
2022-08-26 19:34:54 +0800

    Ready to deploy

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+import os
+
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.flaskenv')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+from . import app
